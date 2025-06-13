521e372fd2b877942fbe786cd0de50697441fb59
manel-bc <manelbc+github@pm.me>
2025-06-13 22:20:51 +0200

    Add install script

---
+++ run_onchange_install_pkgs.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+## THIS SCRIPT MUST BE IDEMPOTENT
+
+function main() {
+    # Install packages
+    yay -S \
+        --noconfirm \
+        firefox \
+        polkit
+        # bat
+        # micro
+        # tldr
+        # navi
+        # fzf
+        # btop
+        # diff-so-fancy
+        # fish
+        # vscodium-bin
+}
+
+function prompt() {
+    msg="$1"; shift
+    echo "$msg"
+    read -p "Press key to continue.. " -n1 -s
+}
+
+function install_yay() {
+    if command -v yay >/dev/null 2>&1
+    then
+        return
+    fi
+
+    sudo pacman -S --noconfirm \
+        --needed git base-devel \
+        && git clone https://aur.archlinux.org/yay-bin.git \
+        && cd yay-bin \
+        && makepkg -si
+}
+
+main "$@"
